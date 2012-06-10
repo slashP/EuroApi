04b4b297d8b20b93bd07393af1c16f5b6a58732c
Per-Kristian Helland <perkristianhelland@gmail.com>
2012-06-10 23:14:33 +0200

    Se andres bet for en gitt kamp

--- EuroApi/Controllers/ViewBetController.cs
+++ EuroApi/Controllers/ViewBetController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EuroApi.Context;
+using EuroApi.Models;
 
 namespace EuroApi.Controllers
 {
@@ -20,5 +21,17 @@ namespace EuroApi.Controllers
             return View(bets);
         }
 
+        public ActionResult Match(int id)
+        {
+            var match = _db.Matches.Find(id);
+            if (match == null) return null;
+            var europeanTime = DateTime.UtcNow.AddHours(2);
+            if(match.Date < europeanTime)
+            {
+                var bets = _db.MatchResultBets.Where(x => x.MatchId == id).ToList();
+                return View(bets);
+            }
+            return View(new List<MatchResultBet>());
+        }
     }
 }
